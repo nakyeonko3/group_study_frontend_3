1dc1a47ee345f4b804d2725dc2518a185833474b
badac <badaclock@gmail.com>
2024-06-03 23:01:19 +0900

    #17 Night_Day_City_animation 과제 완성

--- nakeyonko3/animation_day_night_effects_css/script.js
+++ nakeyonko3/animation_day_night_effects_css/script.js
@@ -1,6 +1,18 @@
 const body = document.querySelector("body");
 const circle = document.querySelector(".circle");
-circle.addEventListener("click", () => {
+
+const toggleDarkMode = () => {
   body.classList.toggle("dark-mode");
   circle.classList.toggle("dark-mode");
-});
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("dark-mode", "true");
+  } else {
+    localStorage.setItem("dark-mode", "false");
+  }
+};
+
+circle.addEventListener("click", toggleDarkMode);
+
+if (localStorage.getItem("dark-mode") === "true") {
+  toggleDarkMode();
+}
