cfd5eac0b27c324cfa5e87b029f3e47066dd6e64
badac <badaclock@gmail.com>
2024-06-04 18:03:04 +0900

    #17 네비바 아래 사진 띄어짐 문제 해결

--- nakeyonko3/changing_navbar_based_scroll_css_javascript/script.js
+++ nakeyonko3/changing_navbar_based_scroll_css_javascript/script.js
@@ -3,7 +3,7 @@ const navbar = document.querySelector("#navbar");
 
 const changeTextToIconWhenScroll = () => {
   const scrollTop = window.scrollY;
-  // console.log(window.screenY);
+
   if (lastScroppTop < scrollTop) {
     navbar.classList.add("scrolled");
   } else {
